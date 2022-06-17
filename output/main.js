aa8b14e90c46b0ce40c86e037e060d092685b8fc
homiii116 <miho.yamagata116@gmail.com>
2022-06-17 12:26:00 +0200

    done

--- output/main.js
+++ output/main.js
@@ -1,14 +1,17 @@
-
 const time = document.getElementById('time');
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const resetButton = document.getElementById('reset');
 
+// 開始時間
 let startTime;
-let timeoutID;
+// 停止時間
 let stopTime = 0;
+// タイムアウトID
+let timeoutID;
 
-function countTime() {
+// 時間を表示する関数
+function displayTime() {
   const currentTime = new Date(Date.now() - startTime + stopTime);
   const h = String(currentTime.getHours()-1).padStart(2, '0');
   const m = String(currentTime.getMinutes()).padStart(2, '0');
@@ -16,17 +19,19 @@ function countTime() {
   const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
 
   time.textContent = `${h}:${m}:${s}.${ms}`;
-  timeoutID = setTimeout(countTime, 10);
+  timeoutID = setTimeout(displayTime, 10);
 }
 
+// スタートボタンがクリックされたら時間を進める
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
   stopButton.disabled = false;
   resetButton.disabled = true;
   startTime = Date.now();
-  countTime();
+  displayTime();
 });
 
+// ストップボタンがクリックされたら時間を止める
 stopButton.addEventListener('click', function() {
   startButton.disabled = false;
   stopButton.disabled = true;
@@ -35,11 +40,12 @@ stopButton.addEventListener('click', function() {
   stopTime += (Date.now() - startTime);
 });
 
+// リセットボタンがクリックされたら時間を0に戻す
 resetButton.addEventListener('click', function() {
   startButton.disabled = false;
   stopButton.disabled = true;
   resetButton.disabled = true;
-  time.textContent = "00:00:00.000";
+  time.textContent = '00:00:00.000';
   stopTime = 0;
 });
 
